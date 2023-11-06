a10e63d73bdf1f5386412d23c87873aef53a31e0
Nesthy Rienald M. Vicente <rienaldnesthy96@gmail.com>
2023-11-06 16:23:36 +0800

    Adding attachment to bulk emailing

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -59,7 +59,67 @@ export const FileUtils = {
     const response = await new Promise(async (resolve, reject) => {
       
 
-      try {
+      //const downloadFile = async (url:any, path:any) => {
+        try {
+          const response = await axios({
+            method: "GET",
+            url: url,
+            responseType: "stream",
+          });
+          // Do something with the response
+          resolve(response)
+        } catch (err) {
+          // Handling errors
+          console.log(err);
+        }
+      //};
+
+
+/*//const fs = require('fs');
+const path = require('path');
+//const axios = require('axios').default;
+
+// fileUrl: the absolute url of the image or video you want to download
+// downloadFolder: the path of the downloaded file on your machine
+const downloadFile = async (fileUrl, downloadFolder) => {
+  // Get the file name
+  const fileName = path.basename(fileUrl);
+
+  // The path of the downloaded file on our machine
+  const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: fileUrl,
+      responseType: 'stream',
+    });
+
+    const w = response.data.pipe(fs.createWriteStream(localFilePath));
+    w.on('finish', () => {
+      console.log('Successfully downloaded file!');
+    });
+  } catch (err) { 
+    throw new Error(err);
+  }
+}; */
+/*
+// Testing
+const IMAGE_URL =
+  'https://www.kindacode.com/wp-content/uploads/2021/01/test.jpg';
+downloadFile(IMAGE_URL, 'download');
+
+const VIDEO_URL =
+  'https://www.kindacode.com/wp-content/uploads/2021/01/example.mp4';
+downloadFile(VIDEO_URL, 'download');
+
+
+
+
+*/
+
+
+
+      /*try {
         const response = await axios.get(url, {
           responseType: 'arraybuffer',
         });
@@ -73,7 +133,7 @@ export const FileUtils = {
         resolve(base64String);
       } catch (err) {
         console.log(err);
-      }
+      }*/
 
 
     });
