ed005fc97da5e8a031615533a84e8a2ac8c8102e
Nesthy Rienald M. Vicente <rienaldnesthy96@gmail.com>
2023-11-13 10:41:51 +0800

    Updated POST /payment/getbookeddates

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1018,7 +1018,7 @@ export const PaymentController = {
 
     var grouped_dates:any = {}
     for(let row in result){
-      if(result[row].status.toUpperCase() === "APPROVED"){
+      if(result[row].status.toUpperCase() === "APPROVED" || result[row].status.toUpperCase() === "PAID"){
         if(!Object.keys(grouped_dates).includes(moment(result[row].booked_date).format("YYYY-MM-DD"))){
           grouped_dates[moment(result[row].booked_date).format("YYYY-MM-DD")] = 1;
         } else{
