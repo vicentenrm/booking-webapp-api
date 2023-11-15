aba17ce56b9684c5cebc9602880c595ee5d0bded
Nesthy Rienald M. Vicente <rienaldnesthy96@gmail.com>
2023-11-15 11:36:59 +0800

    Sending email to customers without slot

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -844,7 +844,7 @@ export const PaymentController = {
       JOIN bookings b ON b.book_id = bi.book_id
       JOIN locations l on l.loc_id = bi.loc_id
       JOIN customers c ON b.cus_id = c.cus_id
-      WHERE status != "Cancelled";`, 
+      WHERE status NOT IN ("Cancelled", "Expired");`, 
       []);
     } else if(role === "APPROVER"){
       sql += SqlString.format(`SELECT c.firstName, c.middleName, c.lastName, c.contactNo, c.emailAddr,
@@ -855,7 +855,7 @@ export const PaymentController = {
       JOIN bookings b ON b.book_id = bi.book_id
       JOIN locations l on l.loc_id = bi.loc_id
       JOIN customers c ON b.cus_id = c.cus_id
-      WHERE status IN ("APPROVED", "REVIEWED", "PAID", "EXPIRED");`, 
+      WHERE status IN ("APPROVED", "REVIEWED", "PAID", "Rejected", "Payment Failed");`, 
       []);
     }
 
