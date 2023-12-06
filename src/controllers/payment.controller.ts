d5f0a7fad72383659f192b67866abb486f6fc216
Nesthy Rienald M. Vicente <rienaldnesthy96@gmail.com>
2023-12-06 15:08:27 +0800

    Debugging

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -907,7 +907,7 @@ export const PaymentController = {
         contactNo: result[row].contactNo,
         emailAddr: result[row].emailAddr,
         refNo: result[row].refNo,
-        booking_date: moment(result[row].created_at).format("YYYY-MM-DD hh:mm:ss"),
+        booking_date: moment.utc(result[row].created_at).format("YYYY-MM-DD hh:mm:ss"),
         productName: result[row].productName,
         totalAmount: result[row].totalAmount,
         booked_date: moment(result[row].booked_date).format("YYYY-MM-DD"),
